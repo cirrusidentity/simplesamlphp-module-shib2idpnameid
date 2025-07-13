a0f5ce1700c28053867fe410a50cd7421cedd05d
Ioannis Igoumenos <ioigoume@gmail.com>
2025-07-13 14:59:20 +0300

    Improve testing

--- tests/lib/Auth/Process/PairwiseIDTest.php
+++ tests/lib/Auth/Process/PairwiseIDTest.php
@@ -5,49 +5,79 @@ declare(strict_types=1);
 namespace Test\SimpleSAML\shib2idpnameid;
 
 use PHPUnit\Framework\TestCase;
-use SimpleSAML\Error\Exception;
 use SimpleSAML\Module\shib2idpnameid\Auth\Process\PairwiseID;
 
 class PairwiseIDTest extends TestCase
 {
+    private array $config = [
+        'attribute' => 'uid',
+        'scope' => 'example.com',
+    ];
+
+    private array $state = [
+        'Attributes' => [
+            'uid' => ['774333']
+        ],
+        'Destination' =>
+            [
+                'entityid' => 'https://somesp.edugain.example.edu/sp'
+            ],
+        'Source' =>
+            [
+                'entityid' => 'https://idp.example.edu/shibboleth'
+            ]
+    ];
+
+    public function testNoConfigOptions(): void
+    {
+        $pairwiseId = new PairwiseID($this->config, null);
+
+        $localState = $this->state;
+        unset($localState['Attributes']);
+        $this->expectExceptionMessage('Missing or invalid attribute array in state.');
+        $pairwiseId->process($localState);
+
+        $localState = $this->state;
+        unset($localState['Source']);
+        $this->expectExceptionMessage('Missing or invalid Source/entityid in state.');
+        $pairwiseId->process($localState);
+    }
+
     /**
-     * @throws Exception
      * @throws \Exception
      */
     public function testPairwiseID()
     {
-        $config = [
-            'attribute' => 'uid',
-            'scope' => 'example.com',
-        ];
-        $pairwiseId = new PairwiseID($config, null);
 
-        $state = [
-            'Attributes' => [
-                'uid' => ['774333']
-            ],
-            'Destination' =>
-                [
-                    'entityid' => 'https://somesp.edugain.example.edu/sp'
-                ],
-            'Source' =>
-                [
-                    'entityid' => 'https://idp.example.edu/shibboleth'
-                ]
-        ];
+        $pairwiseId = new PairwiseID($this->config, null);
+        $localState = $this->state;
 
-        $pairwiseId->process($state);
-        $this->assertArrayHasKey('pairwise-id', $state['Attributes']);
-        $this->assertStringEndsWith('@example.com', $state['Attributes']['pairwise-id'][0]);
+        $pairwiseId->process($localState);
+        $this->assertArrayHasKey('pairwise-id', $localState['Attributes']);
+        $this->assertStringEndsWith('@example.com', $localState['Attributes']['pairwise-id'][0]);
 
         $expectedPairwiseId = $pairwiseId->generatePairwiseID(
-            $state['Attributes'],
+            $localState['Attributes'],
             'uid',
             'https://idp.example.edu/shibboleth',
             'donttellanyone',
             'example.com'
         );
 
-        $this->assertEquals($expectedPairwiseId, $state['Attributes']['pairwise-id'][0]);
+        $this->assertEquals($expectedPairwiseId, $localState['Attributes']['pairwise-id'][0]);
+    }
+
+    /**
+     * @throws \Exception
+     */
+    public function testPairwiseIDFailOnEmptyAttribute()
+    {
+
+        $pairwiseId = new PairwiseID($this->config, null);
+        $localState = $this->state;
+        unset($localState['Attributes']['uid']);
+
+        $this->expectExceptionMessage("Missing or empty attribute: " . $this->config['attribute']);
+        $pairwiseId->process($localState);
     }
 }
